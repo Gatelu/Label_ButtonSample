ab3869753a6f75d206bf3117279ca60b0a13f788
Gate <luchangxiang1@163.com>
2015-07-15 20:24:45 +0800

    Initial Commit

---
+++ Label_ButtonSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Label_ButtonSample
+//
+//  Created by Gate on 15/7/15.
+//  Copyright (c) 2015年 Gate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
